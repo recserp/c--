7a03d4231b8d471751deb4b70ff067648c1b7f7a
recserp <ozussajwan579@gmail.com>
2026-09-01 17:14:38 +0530

    idk

--- fibrecrusion.cpp
+++ fibrecrusion.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 int fib(int nth);
+int fibrecur(int nth);
 int main(){
     int nth;
     std::cout << "nth term";
     std::cin >> nth;
-    std::cout << "output"<<fib(nth);
+    std::cout << "output"<<fib(nth)<<" "<<fibrecur(nth-1);
     return 0;
 }
 
@@ -15,4 +16,12 @@ int fib(int nth){
     }
     int out = array[nth-1];
     return out;
+}
+
+int fibrecur(int nth){
+    if(nth<=1){
+        return nth;
+    }
+    return fibrecur(nth-1) + fibrecur(nth-2);
+    
 }

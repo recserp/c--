f4d8d1c113976a6aacbcb96cd58dec73baa1c7d5
recserp <ozussajwan579@gmail.com>
2026-09-03 15:37:27 +0530

    idk

--- opps.cpp
+++ opps.cpp
@@ -1,35 +1,11 @@
 #include <iostream>
 
-class Human{
+class idk{
     public:
-    std::string name;
-    std::string job;
-    int age;
-
-    void eat(){
-        std::cout << "this person is eating"<<"\n";
-
-    }
-    void work(){
-        std::cout << "this person is working"<<"\n";
-
-    }
-    void sleep(){
-        std::cout << "this person is sleeping";
-    }
+    
 };
+
 int main(){
-
-    Human ozuss;
-    ozuss.name = "ozuss";
-    ozuss.age = 54;
-    ozuss.job = "idk";
-
-    std::cout << ozuss.name <<"\n"<<ozuss.age;
-
-    ozuss.eat();
-    ozuss.work();
-    ozuss.sleep();
 
     return 0;
 }
